ec4a80f36367fe0c838c1a764a173694b8965aba
Dmitriy <D1mk0Leb@yandex.ru>
2023-12-05 09:56:37 +0300

    Adding the pages folder and the base and main files

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,17 @@
 from selenium.webdriver.common.by import By
 
-def test_guest_can_to_login_page(browser):
-    url_link = 'http://selenium1py.pythonanywhere.com/'
-    browser.get(url_link)
+url_link = 'http://selenium1py.pythonanywhere.com/'
+
+# def test_guest_can_to_login_page(browser):
+#     url_link = 'http://selenium1py.pythonanywhere.com/'
+#     browser.get(url_link)
+#     login_link = browser.find_element(By.CSS_SELECTOR, '#login_link')
+#     login_link.click()
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, '#login_link')
     login_link.click()
+
+def test_guest_can_to_login_page(browser):
+    browser.get(url_link)
+    go_to_login_page(browser)
