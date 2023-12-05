fd76962fd3c64f7e77bb1283c44a02f6ea65ec25
Dmitriy <D1mk0Leb@yandex.ru>
2023-12-05 15:02:05 +0300

    Creating the first test and refactoring test_main_page v2

--- test_main_page.py
+++ test_main_page.py
@@ -1,32 +1,12 @@
-<<<<<<< HEAD
 from .pages.main_page import MainPage
 
 from selenium.webdriver.common.by import By
 
 def test_guest_can_go_to_login_page(browser):
     url_link = 'http://selenium1py.pythonanywhere.com/'
-    # инициализиру Page Object, передаю в конструктор экземпляр драйвера и url-адрес:
+    # Инициализирую Page Object, передаю в конструктор экземпляр драйвера и url-адрес:
     page = MainPage(browser, url_link)
-    # открываю страницу:
+    # Открываю страницу:
     page.open()
-    # выполняю метод страницы - перехожу на страницу логина:
+    # Выполняю метод страницы - перехожу на страницу логина
     page.go_to_login_page()
-=======
-from selenium.webdriver.common.by import By
-
-url_link = 'http://selenium1py.pythonanywhere.com/'
-
-# def test_guest_can_to_login_page(browser):
-#     url_link = 'http://selenium1py.pythonanywhere.com/'
-#     browser.get(url_link)
-#     login_link = browser.find_element(By.CSS_SELECTOR, '#login_link')
-#     login_link.click()
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, '#login_link')
-    login_link.click()
-
-def test_guest_can_to_login_page(browser):
-    browser.get(url_link)
-    go_to_login_page(browser)
->>>>>>> b8e4e5f76f1dfc37355871b640064a2a36ccc4d8
