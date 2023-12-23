35c672b1991fbe78b4e4c50341fccea47125abea
Dmitriy <D1mk0Leb@yandex.ru>
2023-12-23 14:48:46 +0300

    Final edits and release of version 1.0

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,17 +1,17 @@
-import pytest
-
 from .base_page import BasePage
 from .locators import BasketPageLocators
 
 class BasketPage(BasePage):
-    def should_be_empty_basket(self):
-        self.should_be_down_if_product_in_page()
-        self.should_be_down_if_not_product_in_page()
 
-    def should_be_down_if_product_in_page(self):
+    def should_be_empty_basket(self):
         assert self.is_not_element_present(*BasketPageLocators.BASKET_PRODUCT), \
             'Корзина не пуста, хотя должна быть пуста'
+        print('Корзина пуста - так и должно быть')
 
-    def should_be_down_if_not_product_in_page(self):
-        assert self.is_element_present(*BasketPageLocators.BASKET_PRODUCT), \
-            'Тест падает потому-что товара нет в корзине'
+
+
+
+
+
+
+
--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,6 @@ import pytest
 import time
 
 from .pages.login_page import LoginPage
-from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 
@@ -19,8 +18,10 @@ URL_DATASET = [
     ['http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9']
 ]
 
-url_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+product_page_link = 'https://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/'
+login_page_link = 'https://selenium1py.pythonanywhere.com/accounts/login/'
 
+@pytest.mark.need_review
 @pytest.mark.parametrize(
     argnames=['url_link'],
     argvalues=URL_DATASET
@@ -29,37 +30,52 @@ def test_guest_can_add_product_to_basket(browser, url_link):
     if url_link == 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7':
         pytest.xfail(f'Страница с неверным товаром: {url_link}')
         return
-    page = MainPage(browser, url_link)
-    page.open()
-    product_page = ProductPage(browser, browser.current_url)
+    product_page = ProductPage(browser, url_link)
+    product_page.open()
     product_page.should_be_product_page()
     product_page.save_name_and_price_product()
     product_page.add_to_basket()
     product_page.solve_quiz_and_get_code()
     product_page.should_be_send_add_to_basket()
 
-@pytest.mark.link
 def test_guest_should_see_login_link_on_product_page(browser):
-    url_link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
-    page = ProductPage(browser, url_link)
-    page.open()
-    page.should_be_login_link()
+    product_page = ProductPage(browser, product_page_link)
+    product_page.open()
+    product_page.should_be_login_link()
 
-@pytest.mark.link
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    url_link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
-    page = ProductPage(browser, url_link)
-    page.open()
-    page.go_to_login_page()
+    product_page = ProductPage(browser, product_page_link)
+    product_page.open()
+    product_page.go_to_login_page()
 
-@pytest.mark.check_empty_basket
+
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    url_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    product_page = ProductPage(browser, url_link)
+    product_page = ProductPage(browser, product_page_link)
     product_page.open()
     product_page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, product_page_link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_not_be_success_message()
+
+def test_guest_see_success_message(browser):
+    product_page = ProductPage(browser, product_page_link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, product_page_link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_disappear_success_message()
 
 @pytest.mark.user_add_product
 class TestUserAddToBasketFromProductPage():
@@ -68,28 +84,27 @@ class TestUserAddToBasketFromProductPage():
         email = str(time.time()) + "@fakemail.org"
         password = str(time.time())
 
-        self.page = ProductPage(browser, url_link)
-        self.page.open()
-        self.page.go_to_login_page()
-        self.login_page = LoginPage(browser, url_link)
+        self.login_page = LoginPage(browser, login_page_link)
+        self.login_page.open()
         self.login_page.should_be_login_page()
         self.login_page.register_new_user(email, password)
         self.login_page.should_be_authorized_user()
 
     def test_user_see_success_message(self, browser):
-        page = ProductPage(browser, url_link)
-        page.open()
-        page.should_not_be_success_message()
+        product_page = ProductPage(browser, product_page_link)
+        product_page.open()
+        product_page.should_not_be_success_message()
 
-    def test_guest_can_add_product_to_basket(self, browser):
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
         url_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
-        page = ProductPage(browser, url_link)
-        page.open()
-        page.should_be_product_page()
-        page.save_name_and_price_product()
-        page.add_to_basket()
-        page.solve_quiz_and_get_code()
-        page.should_be_send_add_to_basket()
+        product_page = ProductPage(browser, url_link)
+        product_page.open()
+        product_page.should_be_product_page()
+        product_page.save_name_and_price_product()
+        product_page.add_to_basket()
+        product_page.solve_quiz_and_get_code()
+        product_page.should_be_send_add_to_basket()
 
 
 
